e52ca022105140836d04ecc6ca1c97db8f8b79f3
Shaama Manoharan <110484017+Shaamam@users.noreply.github.com>
2025-09-11 13:09:51 +0530

    creating project

---
+++ src/test/java/com/bothub/weather_mcp_server/WeatherMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bothub.weather_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
